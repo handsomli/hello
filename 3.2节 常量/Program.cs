57379ee98cb035f7f76058e2a8ea5952ac7dc525
handsomli <handsom_li@163.com>
2020-03-30 20:08:10 +0800

    添加项目文件。

---
+++ 3.2节 常量/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2节_常量
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int p= 5;
+                const double r = 3.14;
+            Console.WriteLine("这是圆的周长" + r * r * p);
+            Console.ReadKey();
+        }
+    }
+}
